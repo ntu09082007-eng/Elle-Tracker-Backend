d8b18a1416bb08bf974f2efe7d36ba166527f433
ntu09082007-eng <ntu09082007@gmail.com>
2026-04-28 12:50:47 +0700

    Refactor getVotes method and remove Cache-Control header

--- src/realtime/realtime.controller.ts
+++ src/realtime/realtime.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Header } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { RealtimeService } from './realtime.service';
 
 @Controller('realtime')
@@ -6,9 +6,8 @@ export class RealtimeController {
   constructor(private readonly realtimeService: RealtimeService) {}
 
   @Get()
-  @Header('Cache-Control', 'public, max-age=4')
   async getVotes() {
-    // 🛠 Dùng getCachedData() vì trong Service của bà tên nó là thế!
-    return await this.realtimeService.getCachedData(); 
+    // Gọi đúng hàm getCachedData
+    return this.realtimeService.getCachedData();
   }
 }
